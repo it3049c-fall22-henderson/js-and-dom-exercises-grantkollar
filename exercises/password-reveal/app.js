0aa0eb6f61131c4d5c0f6c0fb34caaad02bc6c51
grantkollar <89742140+grantkollar@users.noreply.github.com>
2022-09-18 21:12:30 -0400

    added javascript code to the password reveal

--- exercises/password-reveal/app.js
+++ exercises/password-reveal/app.js
@@ -1,7 +1,14 @@
 // 🤖: create 2 constants with references to the password input and the reveal button from the DOM
-
+const password = document.getElementById("password")
+const showPass = document.getElementById("togglePassword")
 
 // 🤖: Create an Event Listener on the password reveal button
+showPass.addEventListener('click',function(){
+if(password.getAttribute("type") == "password")
+	password.setAttribute("type","text");
+else
+	password.setAttribute("type","password")
+});
 // 🤖: The event listener should toggle the password field attribute between `password` and `text` to change the visibility of the password field content.
 
 
